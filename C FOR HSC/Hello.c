ed1005554824c3a819c8f34baf208ca8710b1ec2
@hlam7 <206399212+ahlam26824@users.noreply.github.com>
2025-04-10 19:53:58 +0600

    Add files via upload

---
+++ C FOR HSC/Hello.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(){
+
+    int age = 26;
+    char grade = 'A';
+    float pi = 3.1416;
+
+
+bool isOnline = false ;
+
+if(isOnline){
+    printf("You are online");
+}
+else{
+    printf("You are offline");
+}
+{
+}
+printf("%d\n",isOnline);
+
+    char gmail[] = 'mahfujrohoman117@gmail.com';
+
+    printf("her Age is %d\n ",age);
+    printf("Her Grade is %c\n",grade);
+    printf("The value of pi is %f\n",pi);
+
+    printf("My Mail is %s",gmail);
+
+char mail[] = "mahfujrohoman117@gamil.com" ;
+
+printf("My Mail is %s",mail);
+
+
+
+    return 0;
+}
